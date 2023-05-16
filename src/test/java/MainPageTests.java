731572c42a1ad095b2af6f512eecfb533e07f28d
Nitka04 <nuta0403@ukr.net>
2023-05-16 18:55:06 +0300

    Initial commit

---
+++ src/test/java/MainPageTests.java
@@ -0,0 +1,21 @@
+import org.testng.annotations.Test;
+import pages.MainPage;
+import startProcess.Start;
+
+import java.awt.*;
+
+public class MainPageTests extends Start {
+       @Test
+       public void TestOfBackCallConformation() throws InterruptedException {
+           assertions.equalsOfText(mainPage.CallMeBackGetText("99 999 99 999"), MainPage.Labels.ConformationText);
+
+       }
+
+       @Test(dataProvider = "urlOnlineSupportCheck")
+    public void TestOfMessengerUrl(String urlOnlineSupportCheck){
+            mainPage.getUrlFromOpenWindow();
+
+       }
+
+
+}
